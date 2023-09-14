4ab029610db37f16ccade834bc0328b7d0c5a91e
mr xue <falethe@gmail.com>
2023-09-14 23:32:45 +0800

    图片预览组件

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,6 +3,7 @@ import { getDetail } from '@/apis/detail';
 import {onMounted, ref} from 'vue'
 import {useRoute} from 'vue-router'
 import DetailHotVue from './components/DetailHot.vue';
+import ImageView from "@/components/ImageView/index.vue"
 
 const goods = ref({})
 const route = useRoute()
@@ -36,7 +37,7 @@ onMounted(()=>getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+                <ImageView :image-list="goods.mainPictures"/>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
