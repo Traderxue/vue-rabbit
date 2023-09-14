f40238ba7a0ca7d8d85d4201284b8dfeda58314b
mr xue <falethe@gmail.com>
2023-09-14 11:29:22 +0800

    图片懒加载

--- src/apis/home.js
+++ src/apis/home.js
@@ -5,3 +5,15 @@ export function getBannerApi(){
         url:"home/banner"
     })
 }
+
+export const findNewApi =()=>{
+    return http({
+        url:"/home/new"
+    })
+}
+
+export const getHotApi =()=>{
+    return http({
+        url:"home/hot"
+    })
+}
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import App from './App.vue'
 import router from './router'
 
 import "@/styles/common.scss"
+import { useIntersectionObserver } from '@vueuse/core'
 
 
 //测试接口
@@ -20,3 +21,19 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+//定义全局指令，实现图片懒加载
+app.directive('img-lazy',{
+    mounted(el,binding){
+        // el:指令绑定的元素,binging.balue 指令等于号后面绑定的值 图片的url
+        useIntersectionObserver(
+            el,
+            ([{ isIntersecting }],) => {
+                if(isIntersecting){
+                    //进入了视图区域
+                    el.src=binding.value
+                }
+            },
+          )
+    }
+})
